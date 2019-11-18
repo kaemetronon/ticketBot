5332c1b5ff90651a8eba77cd44e8d5b22f1118a3
kaemetronon <kametronon@mail.ru>
2019-11-18 12:05:26 +0300

    dsky has been added

--- src/main/java/service/Handler.java
+++ src/main/java/service/Handler.java
@@ -15,22 +15,28 @@ public class Handler {
                 String moexTitle = "MOEX";
                 String rstiTitle = "RSTI";
                 String fxgdTitle = "FXGD";
+                String dskyTitle = "DSKY";
 
                 String moexUrl = "https://www.investing.com/equities/moskovskaya-birzha-oao";
                 String rstiUrl = "https://www.investing.com/equities/rosseti-ao";
                 String fxgdUrl = "https://www.investing.com/etfs/finex-physically-held-gold-usd";
+                String dskyUrl = "https://www.investing.com/equities/detskiy-mir-pao";
 
                 Float moexBuyCourse = 95.53F;
                 Float rstiBuyCourse = 1.2436F;
                 Float fxgdBuyCourse = 662.1F;
+                //!!!!!!!
+                Float dskyBuyCourse = 92.88F;
 
                 Integer moexStocksNumber = 20;
                 Integer rstiStocksNumber = 1000;
                 Integer fxgdStocksNumber = 2;
+                Integer dskyStockNumber = 20;
 
                 add(new Ticket(moexTitle, moexUrl, moexBuyCourse, moexStocksNumber));
                 add(new Ticket(rstiTitle, rstiUrl, rstiBuyCourse, rstiStocksNumber));
                 add(new Ticket(fxgdTitle, fxgdUrl, fxgdBuyCourse, fxgdStocksNumber));
+                add(new Ticket(dskyTitle, dskyUrl, dskyBuyCourse, dskyStockNumber));
             }
         };
     }
@@ -104,6 +110,7 @@ public class Handler {
                         calculator.setStocksNumber(ticket.getStocksNumber());
 
                         counter += calculator.getPercentAndAmount()[1];
+                        System.out.println(counter);
                     }
                 }
                 counter = Float.valueOf(Math.round(counter * 10000));
